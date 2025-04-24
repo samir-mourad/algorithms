1ef5237f1c1a58b7e40b5f39dc0717804ddfee75
Samir Mourad <samir.alviani@gmail.com>
2025-04-24 14:52:42 -0300

    Bubble Sort implementation

---
+++ Algorithms.Tests/Sort/BubbleSortTest.cs
@@ -0,0 +1,20 @@
+﻿using Algorithms.Sort;
+
+namespace Algorithms.Tests.Sort
+{
+    public class BubbleSortTest
+    {
+        [Fact]
+        public void Sort()
+        {
+            int[] unsorted = [5, 3, 8, 4, 2];
+            int[] sorted = [2, 3, 4, 5, 8];
+
+            var bubbleSort = new BubbleSort(unsorted);
+
+            var result = bubbleSort.Sort();
+
+            Assert.Equal(sorted, result);
+        }
+    }
+}
---
+++ Algorithms/Sort/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace Algorithms.Sort
+{
+    public class BubbleSort
+    {
+        private readonly int[] _collection = Array.Empty<int>();
+
+        public BubbleSort(int[] collection)
+        {
+            _collection = collection;
+        }
+
+        public int[] Sort()
+        {
+            for (var i = 0; i < _collection.Length; i++)
+            {
+                for (var j = i + 1; j < _collection.Length; j++)
+                {
+                    if (_collection[j] < _collection[i])
+                    {
+                        var temp = _collection[i];
+                        _collection[i] = _collection[j];
+                        _collection[j] = temp;
+                    }
+                }
+            }
+
+            return _collection;
+        }
+    }
+}
